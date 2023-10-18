665523eb9b04d345a922ffc99bb36d37b6a918b9
Denys Levchenko <levchenko.denis323@gmail.com>
2023-10-18 13:32:14 +0200

    Small fixes

--- src/main/java/com/example/orderdemo/repository/OrderRepository.java
+++ src/main/java/com/example/orderdemo/repository/OrderRepository.java
@@ -2,7 +2,9 @@ package com.example.orderdemo.repository;
 
 import com.example.orderdemo.model.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
--- src/main/java/com/example/orderdemo/repository/UserRepository.java
+++ src/main/java/com/example/orderdemo/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.orderdemo.repository;
 import com.example.orderdemo.model.dto.UserDto;
 import com.example.orderdemo.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     void deleteById(UUID id);
